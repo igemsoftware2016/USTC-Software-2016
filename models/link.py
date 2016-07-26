a73928fea2359f9f0e090804262e39530438e840
zzh1996 <zzh1996@mail.ustc.edu.cn>
2016-07-27 00:46:37 +0800

    fix 'Link' primary key problem

--- models/link.py
+++ models/link.py
@@ -3,8 +3,8 @@ from database import TableBase, Column, String, ForeignKey
 
 class Link(TableBase):
     __tablename__ = 'link'
-    link_id = Column(String(256), primary_key=True)
-    node_a_id = Column(String(256), ForeignKey('node.node_id'))
-    node_b_id = Column(String(256), ForeignKey('node.node_id'))
+    link_id = Column(String(256))
+    node_a_id = Column(String(256), ForeignKey('node.node_id'), primary_key=True)
+    node_b_id = Column(String(256), ForeignKey('node.node_id'), primary_key=True)
     link_catalog = Column(String(256))
     link_name = Column(String(256))
