90b02307ce8e54dd1faa7b411959cd3022de6f04
asahi <hty980204@gmail.com>
2016-07-27 11:59:57 +0800

    update yml

--- set_up.py
+++ set_up.py
@@ -1,8 +1,23 @@
-import os
-import unittest2 as unittest
+#!/usr/bin/env python3
+'''
+    Setup all tests files and run pylint
+'''
+import unittest
+import subprocess
+from pylint import epylint as lint
 
+DIR_LIST=[
+    'plugins',
+    'models'
+]
+
+test='123'
 def suite():
     return unittest.TestLoader().discover('tests','test_*.py')
 
 if __name__ == '__main__':
-    unittest.main(defaultTest = 'suite')
+    # unittest.main(defaultTest = 'suite')
+    subprocess.call(['pylint','-rn','set_up.py'])
+    # (pylint_stdout,pylint_stderr)=lint.py_run('test_sample/MyDict.py',return_std=True)
+    # result_string=pylint_stdout.getvalue()
+    # print(result_string)
