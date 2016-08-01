f4ac70bbc15876a8d28f7ebb8fb089dd058cd161
zzh1996 <zzh1996@mail.ustc.edu.cn>
2016-08-02 01:19:59 +0800

    move path_finder2.py to path_finder.py

--- plugins/path_finder.py
+++ plugins/path_finder.py
@@ -5,16 +5,14 @@ from datetime import *
 from database import DBSession
 from models import Node, Link
 
-inf = 10000
-Graph = []
-dis = []
-node_pool = {}
-search_pool = {}
+inf = 100
+Graph = {}
+dis = {}
 node_count = 0
 
 
 class State:
-    def __init__(self, f, g, cur, prepath):  # f is the distance to src, g is the distance to dst
+    def __init__(self, f, g, cur, prepath):    # f is the distance to src, g is the distance to dst
         self.f = f
         self.g = g
         self.cur = cur
@@ -29,8 +27,7 @@ class State:
 
 def calcu_dis(dst, n, maxlen):
     global dis
-    dis = [inf for i in range(n)]
-    visited = [False for i in range(n)]
+    visited = {}
     queue = Queue()
 
     dis[dst] = 0
@@ -40,11 +37,10 @@ def calcu_dis(dst, n, maxlen):
         u = queue.get()
         # visited[u] = False
         for v in Graph[u]:
-            if not visited[v] and dis[v] > dis[u] + 1:
+            if (v not in visited or dis[v] > dis[u] + 1) and dis[u] + 1 <= maxlen:
                 dis[v] = dis[u] + 1
-                if dis[v] < maxlen:
-                    visited[v] = True
-                    queue.put(v)
+                visited[v] = True
+                queue.put(v)
 
 
 def a_star(src, dst, pathnum):
@@ -53,7 +49,7 @@ def a_star(src, dst, pathnum):
     cnt = 0
 
     time_monitor = datetime.now()
-    if dis[src] == inf:  # if there's no path from src to dst
+    if dis[src] == inf:    # if there's no path from src to dst
         yield "None"
         return
     p_queue.put(State(dis[src], 0, src, [src]))
@@ -73,72 +69,32 @@ def a_star(src, dst, pathnum):
             return
 
 
-def reload():  # reload data from database
+def reload():    # reload data from database
     session = DBSession()
-    global node_pool, search_pool, node_count, Graph
+    global node_count, Graph
 
     # construct the graph
     for node in session.query(Node).all():
-        node_pool[
-            node.node_id] = node_count  # match node_id of string type to int type, for the convenience of path finding
-        search_pool[node_count] = node.node_id  # match int to node_id, it is needed when returning results
-        node_count += 1
-        Graph.append([])  # Graph[v] is a list containing the adjacent vertexes of v
+        Graph[node.node_id] = []    # Graph[v] is a list containing the adjacent vertexes of v
     for link in session.query(Link).all():
-        u = node_pool[link.node_a_id]
-        v = node_pool[link.node_b_id]
-        if v not in Graph[u]:
-            Graph[u].append(v)  # add link.node_b_id as a adjvex of node_a_id
-        if u not in Graph[v]:
-            Graph[v].append(u)  # and vice versa
+        if link.node_b_id not in Graph[link.node_a_id]:
+            Graph[link.node_a_id].append(link.node_b_id)    # add link.node_b_id as a adjvex of node_a_id
+        if link.node_a_id not in Graph[link.node_b_id]:
+            Graph[link.node_b_id].append(link.node_a_id)    # and vice versa
 
 
-# Graph[node_pool[link.node_a_id]].append(node_pool[link.node_b_id])
-#        Graph[node_pool[link.node_b_id]].append(node_pool[link.node_a_id])
-
-
-<<<<<<< HEAD
-def path_finder(s, t, k, maxlen, rebuild=False):    # s:starting point, t:terminal point, k:number of paths required
-    time_list = {}
-    if rebuild:    # if it is needed to rebuild the Graph
-=======
-def path_finder(s, t, k, rebuild=False):  # s:starting point, t:terminal point, k:number of paths required
-    time_list = {}
-    time_list['start_time'] = datetime.now()
-    if rebuild:  # if it is needed to rebuild the Graph
->>>>>>> origin/zyx
-        reload()
-        time_list['reload_time'] = datetime.now()
-    s = node_pool[s]
-    t = node_pool[t]
-    time_list['start_time'] = datetime.now()
+def path_finder(s, t, k, maxlen):    # s:starting point, t:terminal point, k:number of paths required
     calcu_dis(t, node_count, maxlen)
-    time_list['calcudis_time'] = datetime.now()
-    path_list = []  # contain the found paths
+    path_list = []    # contain the found paths
     for p in a_star(s, t, k):
         if p == "None" or p == "Timeout":
             break
         else:
-            path = []
-            for node in p:
-                path.append(search_pool[node])
-            path_list.append(path)
-    time_list['a*time'] = datetime.now()
-    print(time_list)
+            path_list.append(p)
     return path_list
 
 
 reload()
 
-'''
-Graph = [[1, 2], [0, 4], [0, 5], [4], [3, 5], [2, 4]]
-calcu_dis(5, 6)
-for p in a_star(1, 5, 2):
-    print(p)
-'''
 
-<<<<<<< HEAD
-# print(path_finder("ECK120011235", "ECK120000311", 5, 5, False))
-=======
-# print(path_finder("ECK120011235", "ECK120000311", 5, 5, False))
->>>>>>> origin/zyx
+print(path_finder("ECK120011235", "ECK120000311", 5, 5))
