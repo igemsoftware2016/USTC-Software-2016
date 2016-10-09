b997be584c71ba29c45f4edb8c6536e5341d18a1
Jing wang <jing@Jings-MacBook-Air.local>
2016-10-09 18:42:46 +0800

    bug fix

--- js/blast.js
+++ js/blast.js
@@ -397,7 +397,7 @@ window.onload=(function () {
         var res_height = 500;
         var svg_container = d3.select('#result_blast').append('svg')
             .attr('width', res_width + 280)
-            .attr('height', res_height + 120)
+            .attr('height', res_height + 180)
             .attr("id","svg_blast");
         
         var svg = svg_container.append("g")
