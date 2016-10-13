5e22fe9e8b2199817a1d6884e0f906ca65aa4c9f
zyx <wjszzyx@sohu.com>
2016-10-13 22:37:57 +0800

    try to fix bug in path_finder again

--- plugins/path_finder.py
+++ plugins/path_finder.py
@@ -38,9 +38,9 @@ def calcu_dis(dst, n, maxlen):
     while not queue.empty():
         u = queue.get()
         # visited[u] = False
-        for u_node in session.query(BioSys).filter(BioSys.id == u):
+        for u_node in session.query(BioSys).filter(BioSys.gene_id == u):
             for node in session.query(BioSys).filter(BioSys.bsid == u_node.bsid):
-                Graph[u].append(node.id)
+                Graph[u].append(node.gene_id)
         for v in Graph[u]:
             if (v not in visited or dis[v] > dis[u] + 1) and dis[u] + 1 <= maxlen:
                 dis[v] = dis[u] + 1
