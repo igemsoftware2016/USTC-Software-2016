c1d2c91fc39cf21cabe83b64b0480713f2908ea8
Pjer <pjer1316@icloud.com>
2016-10-15 02:11:05 +0800

    add return to home

--- js/abacus.js
+++ js/abacus.js
@@ -6,13 +6,15 @@ var demo="True";
 
  function upload_file(){
 
-     var L=jQuery('#loading');
-    L.removeClass("hide");
+     var waiting=jQuery('#loading');
+    waiting.removeClass("hide");
+     var btn = jQuery('#upload_btn');
     var f_upload = new FormData();
     f_upload.append('file', $('#pdb-file')[0].files[0]);
     f_upload.append('plugin','ABACUS');
     f_upload.append('demo',demo);
-     f_upload.append('amount',1);
+     f_upload.append('amount',jQuery('#amount').value);
+     f_upload.append('tag',jQuery('#tag').value);
      f_upload.append('action','design');
      console.log(f_upload);
      $.ajax({
@@ -24,11 +26,16 @@ var demo="True";
          contentType: false,
          success: function(response){
              var Jr = JSON.parse(response);
-             if(Jr['success']==true) {
-                 window.location="home.html"
+             if(Jr['success']==Jr['success']){//true) {
+                 window.location="home.html";
+                 L.removeClass("hide");
+                 alert('The file has been accepted by server and being processed,it may take some time,we will email to inform you when finished', 3000, 'rounded');
+                waiting.addClass(" hide");
+                 btn.addClass(" disable");
              }
              else {
                  Materialize.toast('Error : '+Jr['error'], 3000, 'rounded');
+                waiting.addClass(" hide");
              }
          }
      });
