7563eb70ce06b6c3ff1d58394dcd518b7ea52ff4
Neil Zhao <zzrcxb@gmail.com>
2016-10-18 23:04:44 +0800

    upload

--- plugins/dbupload/updata.py
+++ plugins/dbupload/updata.py
@@ -2,4 +2,9 @@ from .dbprofile import *
 from .uploaddb import *
 
 if __name__ == '__main__':
-    start
+    start = time.clock()
+    print(upload.upload("\srv\All_Data.gene_info", gene_commit, 15, 500000, echo=True, log=True))
+
+    # set_link(path=r"\srv\All_Data.gene_info", echo=True, log=True)
+
+    print(str(time.clock() - start) + 's used.')
