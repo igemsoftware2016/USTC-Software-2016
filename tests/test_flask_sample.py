f908e61e0c27639d7145bfe98a3e0ae44c69297e
Zibo Wang <wzb15@mail.ustc.edu.cn>
2016-10-18 22:27:40 +0800

    some pano tests

--- tests/test_flask_sample.py
+++ tests/test_flask_sample.py
@@ -8,7 +8,7 @@ os.environ['FLASK_TESTING'] = '1'
 import app
 
 
-class FlaskrTestCase(unittest.TestCase):
+class FlaskTestCase(unittest.TestCase):
     def setUp(self):
         app.app.config['TESTING'] = True
         self.app = app.app.test_client()
@@ -30,68 +30,74 @@ class FlaskrTestCase(unittest.TestCase):
         assert b'redirect' in rv.data
 
         # register
-        rv = self.call_plugin(
-            plugin='user_model',
-            action='create_user',
-            email='e',
-            password='123',
-            username='u')
+        rv = self.call_plugin(plugin='user_model', action='create_user', email='e', password='123', username='u')
         assert rv['success']
 
         # email already exists
-        rv = self.call_plugin(
-            plugin='user_model',
-            action='create_user',
-            email='e',
-            password='123',
-            username='u')
+        rv = self.call_plugin(plugin='user_model', action='create_user', email='e', password='123', username='u')
         assert not rv['success']
 
         # wrong password
-        rv = self.call_plugin(
-            plugin='user_model',
-            action='validate_login',
-            email='e',
-            password='1234')
+        rv = self.call_plugin(plugin='user_model', action='validate_login', email='e', password='1234')
         assert not rv['success']
 
         # wrong email
-        rv = self.call_plugin(
-            plugin='user_model',
-            action='validate_login',
-            email='e2',
-            password='123')
+        rv = self.call_plugin(plugin='user_model', action='validate_login', email='e2', password='123')
         assert not rv['success']
 
         # successful login
-        rv = self.call_plugin(
-            plugin='user_model',
-            action='validate_login',
-            email='e',
-            password='123')
+        rv = self.call_plugin(plugin='user_model', action='validate_login', email='e', password='123')
         assert rv['success']
 
         # edit profile
-        rv = self.call_plugin(
-            plugin='user_model',
-            action='edit_profile',
-            username='t1',
-            description='t2',
-            education='t3',
-            major='t4')
+        rv = self.call_plugin(plugin='user_model', action='edit_profile', username='t1', description='t2', education='t3', major='t4')
         assert rv['success']
 
         # get profile
-        rv = self.call_plugin(
-            plugin='user_model',
-            action='get_user_data')
+        rv = self.call_plugin(plugin='user_model', action='get_user_data')
         assert rv['username'] == 't1'
 
     def test_version(self):
-        # get version
-        rv = self.call_plugin(
-            plugin='version')
+        rv = self.call_plugin(plugin='version')
         assert rv['success']
+
+    def test_biobrick(self):
+        rv = self.call_plugin(plugin='biobrick_manager', action='search', key='a')
+        assert rv['success'] and len(rv['list']) > 50
+
+    def test_pano(self):
+        self.call_plugin(plugin='user_model', action='create_user', email='pano1@test', password='x', username='pano1_test')
+        self.call_plugin(plugin='user_model', action='create_user', email='pano2@test', password='x', username='pano2_test')
+        self.call_plugin(plugin='user_model', action='validate_login', email='pano1@test', password='x')
+        rv = self.call_plugin(plugin='pano', action='new', title='a_title', data='a_data', public='false', img='a_img')
+        assert rv['success']
+        a = rv['id']
+        rv = self.call_plugin(plugin='pano', action='new', title='b_title', data='b_data', public='true', img='b_img')
+        assert rv['success']
+        b = rv['id']
+        rv = self.call_plugin(plugin='pano', action='list')
+        assert rv['success'] and set(rv['ids']) == {a, b}
+        rv = self.call_plugin(plugin='pano', action='get_event_data')
+        assert rv['success'] and len(rv['events']) == 1 and rv['events'][0]['project_id'] == b
+        rv = self.call_plugin(plugin='pano', action='load', id=str(a))
+        assert rv['success'] and not rv['public']
+        rv = self.call_plugin(plugin='pano', action='save', id=str(a), title='a_new_title', data='a_data', img='a_img')
+        assert rv['success']
+        rv = self.call_plugin(plugin='pano', action='load', id=str(a))
+        assert rv['success'] and rv['title'] == 'a_new_title'
+        rv = self.call_plugin(plugin='pano', action='delete', id=str(a))
+        assert rv['success']
+        rv = self.call_plugin(plugin='pano', action='load', id=str(a))
+        assert not rv['success']
+        rv = self.call_plugin(plugin='pano', action='list')
+        assert rv['success'] and set(rv['ids']) == {b}
+        rv = self.call_plugin(plugin='pano', action='new', title='c_title', data='c_data', public='false', img='c_img')
+        assert rv['success']
+        c = rv['id']
+        self.call_plugin(plugin='user_model', action='validate_login', email='pano2@test', password='x')
+        rv = self.call_plugin(plugin='pano', action='get_event_data')
+        assert rv['success'] and len(rv['events']) == 1 and rv['events'][0]['project_id'] == b
+        # FIXME Hypercube
 
 
 if __name__ == '__main__':
