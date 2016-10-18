208aa465e01cb7847eaa5e4e40bab3541719172f
Zibo Wang <wzb15@mail.ustc.edu.cn>
2016-10-18 17:07:36 +0800

    add some docstrings

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,5 @@ from .plugin_document import PluginDocument
 from .node import Node
 from .link import Link
 from .user import User
+
+"""Each table in the database and not belonged to a plugin has a model here, so that we can use SQLAlchemy to connect to the database."""
