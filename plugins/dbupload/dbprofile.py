bda1bde75b8cccb669e75faba489cd0d8b0df839
Neil Zhao <zzrcxb@gmail.com>
2016-10-18 11:09:58 +0800

    Add biosys_562 class

--- plugins/dbupload/dbprofile.py
+++ plugins/dbupload/dbprofile.py
@@ -7,6 +7,14 @@ from __init__ import *
 class DataBaseSourceError(Exception):
     pass
 
+# define biosystems of certain taxonomy
+class biosys_single(TableBase):
+    __tablename__ = 'biosys_562'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    tax_id = Column(String(10))
+    gene_id = Column(String(10))
+    bsid = Column(Integer)
+    Symbol = Column(String(64))
 
 # define Gene
 class Gene(TableBase):
